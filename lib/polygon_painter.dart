1e42e3b1e54932da420baef72940a91fe6e38bd2
fahmi <fahmiabdulaziz44@gmail.com>
2023-08-16 14:41:50 +0700

    refactor polygon

--- lib/polygon_painter.dart
+++ lib/polygon_painter.dart
@@ -20,7 +20,8 @@ class PolygonPainter extends CustomPainter {
     final paint = Paint()
       ..strokeWidth = 5
       ..color = Colors.indigoAccent
-      ..style = PaintingStyle.fill;
+      // ..style = PaintingStyle.fill;
+      ..style = PaintingStyle.stroke;
 
     // triangle
     final triangle = Path()
@@ -28,17 +29,20 @@ class PolygonPainter extends CustomPainter {
       ..relativeLineTo(100, 100)
       ..relativeLineTo(-150, 0)
       ..close();
+    canvas.drawPath(triangle, paint);
 
     // square
-    final square1 = Path();
-    square1.moveTo(50, 150);
-    square1.relativeLineTo(100, 0);
-    square1.relativeLineTo(0, 100);
-    square1.relativeLineTo(-100, 0);
-    square1.close();
+    final square1 = Path()
+      ..moveTo(50, 150)
+      ..relativeLineTo(100, 0)
+      ..relativeLineTo(0, 100)
+      ..relativeLineTo(-100, 0)
+      ..close();
+    canvas.drawPath(square1, paint);
 
     // square 2
     const square2 = Rect.fromLTWH(200, 250, 100, 100);
+    canvas.drawRect(square2, paint);
 
     // hexagon
     final hexagon = Path()
@@ -49,6 +53,7 @@ class PolygonPainter extends CustomPainter {
       ..relativeLineTo(-75, -50)
       ..relativeLineTo(0, -75)
       ..close();
+    canvas.drawPath(hexagon, paint);
 
     // Cross
     final cross = Path()
@@ -65,11 +70,6 @@ class PolygonPainter extends CustomPainter {
       ..relativeLineTo(0, -50)
       ..relativeLineTo(50, 0)
       ..close();
-
-    canvas.drawPath(triangle, paint);
-    canvas.drawPath(square1, paint);
-    canvas.drawRect(square2, paint);
-    canvas.drawPath(hexagon, paint);
     canvas.drawPath(cross, paint);
   }
 
